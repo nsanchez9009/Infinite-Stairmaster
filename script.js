24abb16cdcfb6205d0b24815b42ba4679d4b5fbb
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-09-10 15:42:16 -0400

    Beginning script

---
+++ script.js
@@ -0,0 +1,11 @@
+const player = document.querySelector("#player");
+
+player.addEventListener("keyup", (e) => {
+    if (e.code === "Space") {
+
+    }
+
+    else if (e.code === "ControlLeft") {
+
+    }
+});
