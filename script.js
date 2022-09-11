436e034362223df363fc127a75d6e0f4c929f617
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-09-10 20:00:31 -0400

    Imported randomize function from branch adam

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+window.onload = function() {
+    let rowStart = 7;
+    let columnStart = 6;
+    
+    for (let i = 8 ; i > 0 ; i--) {
+
+        const div = document.createElement("div");
+        div.classList.add("platform");
+
+        document.querySelector("#grid").append(div);
+
+        let rand = Math.floor(Math.random() * 2) == 0
+
+        if((rand == 0 && columnStart < 9 ) || columnStart == 1) {
+            columnStart++;
+        }
+
+        else {
+            columnStart--;
+        }
+
+        div.style.gridColumnStart = columnStart;
+        div.style.gridColumnEnd = (columnStart + 1);
+        div.style.gridRowStart = rowStart;
+        div.style.gridRowEnd = (rowStart + 1);
+
+        rowStart--;
+        
+    }
+};
+
 const player = document.querySelector("#player");
 
 //0 is left, 1 is right
