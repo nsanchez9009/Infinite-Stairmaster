77ec916fe8c324fca1008c86d48521156e4ef599
Adam Farrow <adamfarrow1@gmail.com>
2022-09-10 20:51:28 -0400

    linked list to store moves

--- script.js
+++ script.js
@@ -2,22 +2,28 @@ const player = document.querySelector("#player");
 
 //0 is left, 1 is right
 let facing = 1;
-
+let l1;
+let prev = 1;
+let flag = 0;
 window.onload = function() {
     let rowStart = 7;
     let columnStart = 6;
-    
+    l1 = new LinkedList()
+    l1.add(1);
     for(let i = 8 ; i > 0 ; i--){
 
         let div = document.createElement("div");
         div.classList.add("platform");
         document.querySelector("#grid").append(div);
-        console.log(Math.floor(Math.random() * 2))
         let rand = Math.floor(Math.random() * 2) == 0
-        if((rand == 0 && columnStart < 9 ) || columnStart == 1)
+        if((rand == 0 && columnStart < 9 ) || columnStart == 1){
             columnStart++;
-        else
+            l1.add(1);
+        }
+        else{
             columnStart--;
+            l1.add(0);
+        }
 
 
         div.style.gridColumnStart = columnStart;
@@ -27,13 +33,13 @@ window.onload = function() {
         rowStart--;
         
     }
+    l1.printList();
   };
 
 window.addEventListener("keydown", (e) => {
     const styles = window.getComputedStyle(player);
 
-    if (e.code === "Space") {
-        console.log(facing);
+    if (e.code === "Space" && flag == 0) {
         if (facing) {
             moveRight(player, styles);
         }
@@ -41,10 +47,13 @@ window.addEventListener("keydown", (e) => {
             moveLeft(player, styles);
         }
     }
-
-    else if (e.code === "ControlLeft") {
+    else if (e.code === "ControlLeft" && flag == 0) {
         rotate(player, styles);
     }
+
+    if(facing != l1.head.element) flag = 1;
+    console.log(l1.head.element + " " + flag);
+    l1.remove();
 
     //check if grid cell is empty
 });
@@ -110,4 +119,73 @@ function moveUp(player, styles) {
 
 function isEmpty(start, end) {
 
+}
+
+
+
+
+//Linked list implementation
+
+class Node {
+    // constructor
+    constructor(element)
+    {
+        this.element = element;
+        this.next = null
+    }
+}
+
+
+class LinkedList {
+    constructor()
+    {
+        this.head = null;
+        this.size = 0;
+    }
+
+
+    add(element){
+        let node = new Node(element);
+
+        let current;
+ 
+        if (this.head == null)
+            this.head = node;
+        else {
+            current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.size++;
+    }
+
+
+    printList() {
+        let curr = this.head;
+        let str = "";
+        while (curr) {
+            str += curr.element + " ";
+            curr = curr.next;
+        }
+        console.log(str);
+    }
+
+
+    remove(){
+        if(!this.head) return;
+        this.head = this.head.next;
+    }
+ 
+    // functions to be implemented
+    // add(element)
+    // insertAt(element, location)
+    // removeFrom(location)
+    // removeElement(element)
+ 
+    // Helper Methods
+    // isEmpty
+    // size_Of_List
+    // PrintList
 }
